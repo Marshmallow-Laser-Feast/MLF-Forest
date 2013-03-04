5a304fbe8ccf9f114e31aec1217cc6047b040ec6
Marek <laserforest@robinmcnicholas.local>
2013-03-04 11:12:34 +0000

    rod amp fix

--- src/comm/rodStructs.cpp
+++ src/comm/rodStructs.cpp
@@ -137,7 +137,7 @@ void RodInfo::setRawData(const RawAccelerometerData &data) {
 
 
 float RodInfo::getAmplitude() {
-    if(status != ROD_STATUS_OK) return 0;
+    if(status == ROD_STATUS_BAD_ACCELEROMETER) return 0;
     float signal = rawData.z/90.f;
     signal *= ForestSerialPort::ampGain;
     signal = ofClamp(signal, 0, 1);
